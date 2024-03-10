575566aef483f056c58b8ec7745b96635e9d6b1c
Kajal <kajal84142@gmail.com>
2024-03-10 14:44:12 +0530

    first commit

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main(){
+    int a;
+    cout<<"enter a number";
+    cin>>a;
+
+    if(a%2==0)
+    {
+        cout<<"even";
+    }
+    else{
+        cout<<"odd";
+    }
+}
